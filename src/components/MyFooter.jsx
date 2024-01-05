7174d26379b31b9e5c2c3c5c73c547a3aefe54e8
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-01-05 05:21:09 -0800

    footer fixed

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -46,9 +46,9 @@ const MyFooter = () => {
           </div>
         </div>
         <Footer.Divider />
-        <div className="w-full flex justify-round p-4 sm:flex sm:items-center sm:justify-between">
+        <div className="w-full flex justify-between flex-row sm:flex-col justify-round p-4 sm:flex sm:items-center sm:justify-between">
           <Footer.Copyright href="#" by="Flowbite™" year={2022} />
-          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+          <div className=" flex space-x-6 sm:mt-0 sm:justify-center">
             <Footer.Icon href="#" icon={BsFacebook} />
             <Footer.Icon href="#" icon={BsInstagram} />
             <Footer.Icon href="#" icon={BsTwitter} />
